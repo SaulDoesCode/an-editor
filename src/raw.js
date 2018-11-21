53ec072156544fa61374ceb9484b030b4f900d35
Saul van der Walt <saulvdw@gmail.com>
2018-11-21 19:15:09 +0200

    starting over is hard

--- src/raw.js
+++ src/raw.js
@@ -1,48 +1,68 @@
-const {emitter, $, run, dom} = rilti
-const {div, span, article, pre, code} = dom
+const {component, emitter, $, run, dom} = rilti
+const {div, span, section, article, pre, code} = dom
 
-const editor = api => {
-  const editor = emitter()
-  const state = editor.state = List()
-  const segments = editor.segments = List()
-
-  const change = c => {
-    state.push(c)
-    editor.emit.change(c)
-  }
-  Object.assign(change, {
-    Write: 'write',
-    Delete: 'delete',
-    Enter: 'enter',
-    Paste: 'paste'
-  })
-
-  const write = (txt, ops) => {
-    const seg = {txt, ...ops}
-    segments.push(seg)
-    if (api.render) api.render(segments)
-    change({
-      type: change.Write,
-      seg
-    })
-  }
-
-  return Object.assign(editor, {write})
-}
-
-const ED = $('an-editor')
-ED.css('white-space', 'pre')
-
-const TE = editor({
-  render(segments) {
-    ED.textContent = ''
-    console.log(segments)
-    for (const {value: {txt}} of segments) {
-      console.log(txt)
-      ED.innerHTML += txt + '</br>'
+component('an-editor', {
+  props: {
+    state: () => {
+      const state = emitter(List())
+      return state
     }
+  },
+  methods: {
+    newSeg(e, ops) {
+
+    },
+    removeSeg(e, ) {
+
+    }
+  },
+  create(editor) {
+    const {state} = editor
+    
+    editor.pad = section.pad({
+      $: editor,
+      contentEditable: true,
+      onpaste(e) {
+        if (!e.clipboardData || !e.clipboardData.getData) return
+        let data = e.clipboardData.getData('text/plain')
+        if (!data.length) return
+        haltevt(e)
+        // ...
+      },
+      onkeydown(e) {
+        const {key} = e
+        const Enter = key === 'Enter'
+        const Backspace = key === 'Backspace'
+        const ArrowUp = key === 'ArrowUp'
+        const ArrowDown = key === 'ArrowDown'
+        if (Enter || Backspace || ArrowUp || ArrowDown) {
+          haltevt(e)
+
+        }
+      }
+    })
   }
 })
 
-TE.write(`# This is the first line`)
-TE.write(`this is the second line in this text`)
+component('seg-ment', {
+  methods: {
+    select(s, start, end = start) {
+      if (start == null) return selection(s)
+      if (s.txt.length < end) end = s.txt.length
+      selection(s, start, end)
+      return s
+    }
+  },
+  create(seg) {
+    const editor = $(seg.parentElement)
+    seg.on({
+      click() {
+        seg.focus()
+        editor.active = seg
+      },
+      focus() {
+        editor.active = seg
+      }
+    })
+  }
+})
